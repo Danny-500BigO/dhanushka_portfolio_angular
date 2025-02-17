1e50892b819856b4aeaeb7aef336ec3a9ab2ee44
Danny-500BigO <dannybandara500@gmail.com>
2025-02-17 20:24:28 -0330

    build project

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2LBGRJGG.mjs";import"./chunk-RFOP5627.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-OQ3OJJAK.mjs";import"./chunk-RFOP5627.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
