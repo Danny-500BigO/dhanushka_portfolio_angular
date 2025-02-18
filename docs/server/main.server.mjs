d14dc3f8d8a8495c28d3063cc07d12048452d931
Danny-500BigO <dannybandara500@gmail.com>
2025-02-17 21:27:12 -0330

    fix the image path

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OQ3OJJAK.mjs";import"./chunk-RFOP5627.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7F5Q4PY2.mjs";import"./chunk-RFOP5627.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
